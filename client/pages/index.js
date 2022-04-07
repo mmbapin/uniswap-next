b8d25917f355e9df7fb0c2355b12cbf40a267af9
mmbapin <bapinmm@gmail.com>
2022-04-08 03:13:58 +0600

    new repo

---
+++ client/pages/index.js
@@ -0,0 +1,18 @@
+import Header from "../components/Header";
+import Main from "../components/Main"
+
+
+
+export default function Home(){
+  const style = {
+    wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col`
+  }
+  return (
+    <div className={style.wrapper}>
+      <Header />
+      <Main />
+    </div>
+  )
+}
+
+
